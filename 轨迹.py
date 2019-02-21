4841da08a00026676a0415914c7219bb364b1499
billl <billl@synnex.com>
2019-02-21 18:06:18 +0800

    测试文件

--- 轨迹.py
+++ 轨迹.py
@@ -28,6 +28,28 @@ def get_tracks(distance, seconds, ease_func):
     return offsets, tracks
 
 
-a, b = get_tracks(120, 3, 'ease_out_expo')
+a, b = get_tracks(138, 3, 'ease_out_expo')
 print(a, b)
+
+
+def get_tracks(self, distance):
+    print(distance)
+    distance += 20
+    v = 0
+    t = 0.2
+    forward_tracks = []
+    current = 0
+    mid = distance * 3 / 5  # 减速阀值
+    while current < distance:
+        if current < mid:
+            a = 2  # 加速度为+2
+        else:
+            a = -3  # 加速度-3
+        s = v * t + 0.5 * a * (t ** 2)
+        v = v + a * t
+        current += s
+        forward_tracks.append(round(s))
+
+    back_tracks = [-3, -3, -2, -2, -2, -2, -2, -1, -1, -1]
+    return {'forward_tracks': forward_tracks, 'back_tracks': back_tracks}
 
