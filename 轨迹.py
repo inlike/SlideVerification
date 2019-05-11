023a76c108a599fa4c37c2875021260c081af6b6
Python之战 <40748590+dhfjcuff@users.noreply.github.com>
2019-05-11 22:20:08 +0800

    Update 轨迹.py

--- 轨迹.py
+++ 轨迹.py
@@ -2,54 +2,60 @@ import numpy as np
 import math
 
 
-def ease_out_quad(x):
-    return 1 - (1 - x) * (1 - x)
-
-
-def ease_out_quart(x):
-    return 1 - pow(1 - x, 4)
-
-
 def ease_out_expo(x):
+    """
+    曲线函数
+    :param x:
+    :return:
+    """
     if x == 1:
         return 1
     else:
         return 1 - pow(2, -10 * x)
 
 
-def get_tracks(distance, seconds, ease_func):
-    tracks = [0]
-    offsets = [0]
-    for t in np.arange(0.0, seconds, 0.1):
-        ease = globals()[ease_func]
-        offset = round(ease(t/seconds) * distance)
-        tracks.append(offset - offsets[-1])
-        offsets.append(offset)
+def get_tracks(distance, seconds):
+    """
+    轨迹生成函数
+    :param distance: 滑动总距离
+    :param seconds: 滑动总时间
+    :return:
+    """
+    tracks = [0]  # 存放轨迹的数组
+    offsets = [0]  # 存放滑动总距离的记录数组
+    for t in np.arange(0.0, seconds, 0.1):  # 产生一个数列如[0.0, 0.1, 0.2, 0.3]
+        offset = round(ease_out_expo(t/seconds) * distance)  # 根据时间t计算在曲线上的滑动距离
+        tracks.append(offset - offsets[-1])  # 本次计算的距离减去上一次移动的距离，得到本次的轨迹
+        offsets.append(offset)  # 至本次滑动了的总距离
     return offsets, tracks
 
 
-a, b = get_tracks(138, 3, 'ease_out_expo')
+a, b = get_tracks(138, 3)
 print(a, b)
 
 
-def get_tracks(distance):
-    print(distance)
-    distance += 20
-    v = 0
-    t = 0.2
-    forward_tracks = []
-    current = 0
-    mid = distance * 3 / 5  # 减速阀值
-    while current < distance:
-        if current < mid:
+def get_tracksb(distance):
+    """
+    根据物理的先加速再减速规律计算
+    :param distance:
+    :return:
+    """
+    distance += 20  # 加上20是为了滑动超过缺口再回滑
+    v = 0  # 初速度
+    t = 0.2  # 以0.2秒为一个计算周期
+    forward_tracks = []  # 轨迹记录数组
+    current = 0  # 初始移动距离
+    mid = distance * 3 / 5  # 减速阀值即五分之三的距离加速剩下距离减速
+    while current < distance:  # 总移动距离等于输入距离时结束
+        if current < mid:  # 加速状态
             a = 2  # 加速度为+2
-        else:
+        else:  # 减速状态
             a = -3  # 加速度-3
-        s = v * t + 0.5 * a * (t ** 2)
-        v = v + a * t
-        current += s
-        forward_tracks.append(round(s))
+            
+        s = v * t + 0.5 * a * (t ** 2)  # 计算0.2秒周期内的位移
+        v = v + a * t  # 计算本次周期后的速度
+        current += s  # 将之前移动的总距离，加上本次0.2秒周期内移动的距离
+        forward_tracks.append(round(s))  # 记录本次0.2秒周期内的移动距离为轨迹
 
-    back_tracks = [-3, -3, -2, -2, -2, -2, -2, -1, -1, -1]
+    back_tracks = [-3, -3, -2, -2, -2, -2, -2, -1, -1, -1]  # 手动将开头加上的20，生成减去轨迹，即回滑轨迹
     return {'forward_tracks': forward_tracks, 'back_tracks': back_tracks}
-
